d1313fc6f1aafa11e5a20ea01f31f89dce5e780b
evan hahn <evan.hahn@me.com>
2020-08-12 16:36:04 -0700

    adapt layout

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -7,11 +7,13 @@ import styles from './LandingPage.module.css'
 
 const LandingPage = () => {
 	return (
-		<div>
-			<TopNav />
-			<img src={logo} className={styles.logo} alt='logo' />
-			<SearchBar />
-			<SearchSuggestions />
+		<div className={styles.landing}>
+			<div className={styles['search-area']}>
+				<TopNav />
+				<img src={logo} className={styles.logo} alt='logo' />
+				<SearchBar />
+				<SearchSuggestions />
+			</div>
 		</div>
 	)
 }
