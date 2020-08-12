ba6e7b10df20613ee1f5259a95b89861294e7c13
evan hahn <evan.hahn@me.com>
2020-08-12 12:57:05 -0700

    add top nav and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import LandingPage from './LandingPage/LandingPage'
 
 const App = () => {
 	return (
 		<div className='App'>
-			<h1>Hello!</h1>
+			<LandingPage />
 		</div>
 	)
 }
