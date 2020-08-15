4d9cd475fd126b63ed3acab075259ee3cb0ae850
evan hahn <evan.hahn@me.com>
2020-08-15 12:59:15 -0700

    fetch businesses with yelp api

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -4,19 +4,26 @@ import SubNav from '../NavBar/SubNav/SubNav'
 import SearchResultsSummary from './SearchResultsSummary/SearchResultsSummary'
 import SearchResults from './SearchResults/SearchResults'
 import useReactRouter from 'use-react-router'
+import { useBusinessSearch } from '../Hooks/yelp-api/UseBusinessSearch'
 
 const Search = () => {
 	const { location } = useReactRouter()
 	const params = new URLSearchParams(location.search)
-	const termParam = params.get('find_desc')
+	const term = params.get('find_desc')
 	const locationParam = params.get('find_loc')
+	const [
+		businesses,
+		amountResults,
+		searchParams,
+		setSearchParams,
+	] = useBusinessSearch(term, locationParam)
 
 	return (
 		<div>
-			<NavBar term={termParam} location={locationParam} />
+			<NavBar term={term} location={locationParam} />
 			<SubNav />
-			<SearchResultsSummary term={termParam} location={locationParam} />
-			<SearchResults />
+			<SearchResultsSummary term={term} location={locationParam} />
+			<SearchResults businesses={businesses} />
 		</div>
 	)
 }
--- src/Search/SearchResults/SearchResults.js
+++ src/Search/SearchResults/SearchResults.js
@@ -2,13 +2,16 @@ import React from 'react'
 import SearchResult from './SearchResult/SearchResult'
 import styles from './SearchResults.module.css'
 
-const SearchResults = () => {
-	return (
-		<div className={styles['search-results']}>
-			<SearchResult />
-			<SearchResult />
-		</div>
-	)
+const SearchResults = (props) => {
+	if (!props.businesses || !props.businesses.length) {
+		return <div></div>
+	}
+
+	const SearchResults = props.businesses.map((b) => (
+		<SearchResult key={b.id} businesses={b} />
+	))
+
+	return <div className={styles['search-results']}>{SearchResults}</div>
 }
 
 export default SearchResults
